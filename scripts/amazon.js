6af58d3ab779210635c07187db9569060b810d21
LawrenceMensah <lawrencekwekumensah2005@gmail.com>
2026-01-20 13:20:19 +0000

    Add files via upload

---
+++ scripts/amazon.js
@@ -0,0 +1,89 @@
+import { cart, addToCart, showInCart } from "../data/carts.js";
+import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
+
+let productsHTML = '';
+
+products.forEach((product) => {
+  productsHTML += `
+        <div class="product-container">
+          <div class="product-image-container">
+            <img class="product-image"
+              src="${product.image}">
+          </div>
+
+          <div class="product-name limit-text-to-2-lines">
+            ${product.name}
+          </div>
+
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
+            <div class="product-rating-count link-primary">
+              ${product.rating.count}
+            </div>
+          </div>
+
+          <div class="product-price">
+            $${formatCurrency(product.priceCents)}
+          </div>
+
+          <div class="product-quantity-container">
+            <select class="js-quantity-selector-${product.id}">
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart js-added-to-cart-${product.id}">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+          data-product-id="${product.id}">
+            Add to Cart
+          </button>
+        </div>`;
+});
+
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
+JSON.parse(localStorage.getItem('cartUpdate'));
+
+function updateCartQuantity(productId) {
+  showInCart();
+  //This is for the Added cart message
+  const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+
+  let cartReveal;
+  clearTimeout(cartReveal);
+
+  addedMessage.classList.add('added-to-cart-reveal');
+
+  cartReveal = setTimeout(() => {
+    addedMessage.classList.remove('added-to-cart-reveal');
+  }, 2000);
+}
+
+showInCart();
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click', () => {
+    const { productId } = button.dataset;
+
+    addToCart(productId);
+
+    updateCartQuantity(productId);
+
+  });
+});
